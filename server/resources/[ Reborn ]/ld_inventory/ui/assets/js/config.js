dfa4a3d3cb900e0deac8db66f944fc02b853d61f
Reborn-Studios <allysonbedim99@gmail.com>
2024-08-28 18:35:06 -0300

    remove: ld_inventory

--- server/resources/[ Reborn ]/ld_inventory/ui/assets/js/config.js
+++
@@ -1,2 +0,0 @@
-var imgAdress = "http://212.18.114.101/imagens/";
-var imgType = "png";
--- server/resources/[ Reborn ]/ld_inventory/ui/recipes.js
+++
@@ -1,6 +0,0 @@
-RecipeList = {
-  knife: [
-    { name: "aluminum", amount: 2, image: "aluminum.png", label: "Alumínio" },
-    { name: "gunpowder", amount: 2, image: "gunpowder.png", label: "Polvora" },
-  ],
-};
